9c3f86efc08d0b13058b78b00a459f77d1952f9d
raghav-33 <Raghavdevgan333@gmail.com>
2026-04-27 14:43:13 +0530

    Update state.py

--- src/state.py
+++ src/state.py
@@ -4,7 +4,7 @@ from langchain_core.messages import BaseMessage
 
 class AgentState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]  # Mantaing Conversation history
-    category: str # [billing,Genral,Technical]
+    category: str # [billing,Faq,Genral,Technical]
     is_angry: bool # Checking User Behaviour (Angry or not)
     is_resolved: bool # Final Evalution Query got Resolved or Not
     generated_response: str # Generated Response to user Query
