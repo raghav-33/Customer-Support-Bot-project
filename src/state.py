aa17ec3a241dcb034e0b51ff2c648e649173b12d
raghav-33 <Raghavdevgan333@gmail.com>
2026-04-27 14:41:59 +0530

    Update state.py

--- src/state.py
+++ src/state.py
@@ -3,8 +3,8 @@ from langgraph.graph.message import add_messages
 from langchain_core.messages import BaseMessage
 
 class AgentState(TypedDict):
-    messages: Annotated[list[BaseMessage], add_messages] 
-    category: str
-    is_angry: bool
-    is_resolved: bool
-    generated_response: str
+    messages: Annotated[list[BaseMessage], add_messages]  # Mantaing Conversation history
+    category: str # [billing,Genral,Technical]
+    is_angry: bool # Checking User Behaviour (Angry or not)
+    is_resolved: bool # Final Evalution Query got Resolved or Not
+    generated_response: str # Generated Response to user Query
